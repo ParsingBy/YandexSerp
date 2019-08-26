c663e3180800ad078ad8ac0ca4be3155f972936a
BooBoo <denis_ratnikov@list.ru>
2019-08-26 15:27:12 +0300

    migration alter

--- database/migrations/2018_01_01_0000_yandex_serp.php
+++ database/migrations/2018_01_01_0000_yandex_serp.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateYandexSerpTable extends Migration
+class YandexSerp extends Migration
 {
     /**
      * Run the migrations.
@@ -14,21 +14,24 @@ class CreateYandexSerpTable extends Migration
     public function up()
     {
         Schema::create('parsing_by_yandex_serp', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('region_id', 7);
+            $table->bigIncrements('id')->autoIncrement();
+            $table->smallInteger('region_id');
             $table->string('phrase');
-            $table->integer('fetch_result_pages_count', 2);            
+            $table->smallInteger('fetch_result_pages_count');            
             $table->enum('status', ['new', 'in_progress', 'done'])->default('new');
             $table->text('result'); 
         });
 
         Schema::create('parsing_by_yandex_serp_jobs', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->bigInteger('data_id');
-            $table->foreign('data_id')->references('id')->on('parsing_by_yandex_serp')->onDelete('cascade');
-            $table->integer('page', 2);
+            $table->bigIncrements('id')->autoIncrement();
+            $table->bigInteger('data_id')->unsigned();
+            $table->smallInteger('page');
             $table->enum('status', ['new', 'done'])->default('new');
-        });        
+        });    
+
+        Schema::table('parsing_by_yandex_serp_jobs', function($table) {
+           $table->foreign('data_id')->references('id')->on('parsing_by_yandex_serp')->onDelete('cascade');
+        });    
     }
 
     /**
